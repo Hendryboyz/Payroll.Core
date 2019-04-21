9ecec1af11c17775528721b4073ad31cdaa3c52f
Henry Chou <Hendry.js1247@gmail.com>
2019-04-21 14:11:26 +0800

    Add error handling to sales receipts transaction

--- Payroll.Core.Host/SalesReceiptTransaction.cs
+++ Payroll.Core.Host/SalesReceiptTransaction.cs
@@ -18,9 +18,25 @@ namespace Payroll.Core.Host
         public void Execute()
         {
             Employee e = PayrollRepository.GetEmployee(_employeeId);
-            CommissionedClassification cc = e.Classification as CommissionedClassification;
-            SalesReceipt sr = new SalesReceipt(_employeeId, _workingDate, _amount);
-            cc.AddSalesReceipt(sr);
+            bool isValidEmployee = e != null;
+            if (isValidEmployee)
+            {
+                CommissionedClassification cc = e.Classification as CommissionedClassification;
+                SalesReceipt sr = new SalesReceipt(_employeeId, _workingDate, _amount);
+                bool isCommenssionedEmployee = cc != null;
+                if (isCommenssionedEmployee)
+                {
+                    cc.AddSalesReceipt(sr);
+                }
+                else
+                {
+                    throw new InvalidOperationException("non-commenssioned employee");
+                }
+            }
+            else 
+            {
+                throw new InvalidOperationException("No such employee.");
+            }
         }
     }
 }
--- Payroll.Core.Test/SalesReceiptTransactionTests.cs
+++ Payroll.Core.Test/SalesReceiptTransactionTests.cs
@@ -38,5 +38,50 @@ namespace Payroll.Core.Test
             sr.Amount.Should().Be(200);
             #endregion
         }
+
+        [Test]
+        public void GivenNotExistingEmployeeId_WhenExecuteAddSalesReceiptTransaction_ThenThrowInvalidOperationException()
+        {
+            #region Arrange
+            int employeeId = 1000;
+            DateTime workingDate = new DateTime(2019, 4, 21);
+            SalesReceiptTransaction srt = new SalesReceiptTransaction(
+                employeeId, workingDate, 200
+            );
+            #endregion
+
+            #region Action
+            Exception ex = Assert.Catch<Exception>(() => srt.Execute());
+            #endregion
+
+            #region Assert
+            ex.Should().BeOfType<InvalidOperationException>();
+            ex.Message.Should().Contain("No such employee.");
+            #endregion
+        }
+
+        [Test]
+        public void GivenNotCommenssionedEmployee_WhenExecuteAddSalesReceiptTransaction_ThenThrowInvalidOperationException()
+        {
+            #region Arrange
+            int employeeId = 8;
+            AddSalariedEmployee t = new AddSalariedEmployee(employeeId, "user", "home", 1000);
+            t.Execute();
+
+            DateTime workingDate = new DateTime(2019, 4, 21);
+            SalesReceiptTransaction srt = new SalesReceiptTransaction(
+                employeeId, workingDate, 200
+            );
+            #endregion
+
+            #region Action
+            Exception ex = Assert.Catch<Exception>(() => srt.Execute());
+            #endregion
+
+            #region Assert
+            ex.Should().BeOfType<InvalidOperationException>();
+            ex.Message.Should().Contain("non-commenssioned employee");
+            #endregion
+        }
     }
 }
